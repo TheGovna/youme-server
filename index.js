654e658fce0673b4913f44ca09c1621147d1794a
Melissa Thai <mthai95@gmail.com>
2017-02-01 21:06:20 -0800

    Added CORS header

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var fs = require('fs');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// CORS
+// https://www.html5rocks.com/en/tutorials/cors/
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res) {
   res.send('<h1>Hello world</h1>');
 });
